396635d889c5cdd723713bc19c07c4ca2dbb4a42
dsebastian55 <sebaduranb@gmail.com>
2018-05-08 13:16:35 -0300

    little changes in CRUD form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,21 +4,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def show
-    require 'rubygems'
-    require 'net/ssh'
-
-    @hostname = ''
-    @username = ''
-    @password = ''
-    @cmd = "ls"
-
-    begin
-      ssh = Net::SSH.start(@hostname, @username, :password => @password)
-      @res = ssh.exec!(@cmd)
-      ssh.close
-    rescue
-      puts "Unable to connect to #{@hostname} using #{@username}/#{@password}"
-    end
-  end
 end
