3a6fd1385ff3cf6282d30f506f307ebb5fe12b03
dsebastian55 <sebaduranb@gmail.com>
2018-05-14 09:48:25 -0400

    merge migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,31 @@
 
 ActiveRecord::Schema.define(version: 2018_05_13_233233) do
 
+  create_table "access_group_bus_stops", force: :cascade do |t|
+    t.integer "accessGroup_id"
+    t.integer "busStop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["accessGroup_id"], name: "index_access_group_bus_stops_on_accessGroup_id"
+    t.index ["busStop_id"], name: "index_access_group_bus_stops_on_busStop_id"
+  end
+
+  create_table "access_group_services", force: :cascade do |t|
+    t.integer "accessGroup_id"
+    t.integer "service_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["accessGroup_id"], name: "index_access_group_services_on_accessGroup_id"
+    t.index ["service_id"], name: "index_access_group_services_on_service_id"
+  end
+
+  create_table "access_groups", force: :cascade do |t|
+    t.string "Name"
+    t.string "Description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "access_points", force: :cascade do |t|
     t.string "mac"
     t.string "ip"
@@ -27,6 +52,63 @@ ActiveRecord::Schema.define(version: 2018_05_13_233233) do
     t.integer "access_point_id", null: false
     t.index ["access_point_id"], name: "index_access_points_devices_on_access_point_id"
     t.index ["device_id"], name: "index_access_points_devices_on_device_id"
+  end
+
+  create_table "bus_events", force: :cascade do |t|
+    t.integer "bus_id"
+    t.integer "busStop_id"
+    t.datetime "EventTime"
+    t.decimal "BusSpeed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["busStop_id"], name: "index_bus_events_on_busStop_id"
+    t.index ["bus_id"], name: "index_bus_events_on_bus_id"
+  end
+
+  create_table "bus_services", force: :cascade do |t|
+    t.integer "service_id"
+    t.integer "bus_id"
+    t.integer "operator_id"
+    t.datetime "CapturedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_id"], name: "index_bus_services_on_bus_id"
+    t.index ["operator_id"], name: "index_bus_services_on_operator_id"
+    t.index ["service_id"], name: "index_bus_services_on_service_id"
+  end
+
+  create_table "bus_stop_congestions", force: :cascade do |t|
+    t.integer "busStop_id"
+    t.datetime "EventTime"
+    t.decimal "AmountOfPeople"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["busStop_id"], name: "index_bus_stop_congestions_on_busStop_id"
+  end
+
+  create_table "bus_stop_services", force: :cascade do |t|
+    t.integer "busStop_id"
+    t.integer "service_id"
+    t.string "LastArrivalEstimation"
+    t.datetime "CapturedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["busStop_id"], name: "index_bus_stop_services_on_busStop_id"
+    t.index ["service_id"], name: "index_bus_stop_services_on_service_id"
+  end
+
+  create_table "bus_stops", force: :cascade do |t|
+    t.string "Code"
+    t.string "Location"
+    t.string "Name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "buses", force: :cascade do |t|
+    t.string "BusPlateNumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "devices", force: :cascade do |t|
@@ -60,6 +142,34 @@ ActiveRecord::Schema.define(version: 2018_05_13_233233) do
     t.datetime "updated_at", null: false
     t.integer "file_package_id"
     t.index ["file_package_id"], name: "index_file_to_updates_on_file_package_id"
+  end
+
+  create_table "operators", force: :cascade do |t|
+    t.string "Name"
+    t.integer "OperatorNumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "RouteCode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribed_customers", force: :cascade do |t|
+    t.integer "accessGroup_id"
+    t.string "Name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["accessGroup_id"], name: "index_subscribed_customers_on_accessGroup_id"
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.string "Name"
+    t.string "Description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
